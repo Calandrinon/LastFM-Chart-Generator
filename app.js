59e9a6484b49dade06b120e6f661e97dd52e5f81
Calandrinon <georgesut@yahoo.com>
2020-11-16 19:16:22 +0100

    Complete Last.FM API JSON request feature

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 let username = undefined;
-let timespan= undefined;
 let grid_size = undefined;
+
+
+function request_chart_json() {
+    let api_key = config.MY_KEY; 
+    let chart_option = undefined;
+    let url = `http://ws.audioscrobbler.com/2.0/?method=user.getweeklyalbumchart&user=${username}&api_key=${api_key}&format=json`;
+
+    $.getJSON(url, function(data) {
+        console.log(data);
+    });
+}
+
 
 function get_preferences() {
     username = document.getElementById("username").value;
-    timespan = document.getElementById("timespan").value;
     grid_size = document.getElementById("gridsize").value;
-    console.log(`${username} ${timespan} ${grid_size}`);
+    console.log(`${username} ${grid_size}`);
 }
 
 
 function main() {
     get_preferences();
+    request_chart_json();
 }
 
 main();
