d1abc3ef1673a1c939ce1258cd591725b3dfaef6
Calandrinon <georgesut@yahoo.com>
2020-11-17 15:12:43 +0100

    Get links to album art covers
    The album covers can't be drawn yet, there is some bug with the promises

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
 let username = undefined;
 let grid_size = undefined;
 
+function build_chart_image(chart_object) {
+    let canvas = document.getElementById("chart_canvas");
+    let context = canvas.getContext("2d");
+    let albums = {
+        images: []
+    };
+    let api_key = config.MY_KEY; 
+
+    let dummy_image = new Image();
+    dummy_image.src = "https://lastfm.freetls.fastly.net/i/u/174s/dfb8c764b71741e0c1652f197daacd4a.png";
+    let image_width = dummy_image.width;
+    let image_height = dummy_image.height;
+    
+    for (let album_index = 0; album_index < grid_size * grid_size; album_index++) {
+        let album_cover = new Image();
+        let artist_name = chart_object[album_index].artist["#text"];
+        let album_title = chart_object[album_index].name.replace(/ /g, "+");
+        let url = `http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${api_key}&artist=${artist_name}&album=${album_title}&format=json`;
+
+        fetch(url).
+            then(res => res.json()).
+            then(data => console.log(data.album.image[2]["#text"]));
+                //albums.images.push(data.album.image[2]["#text"]);
+        //console.log(albums);
+    } 
+
+    let index = 0;
+    for (let i = 0; i < grid_size; i++) {
+        for (let j = 0; j < grid_size; j++) {
+            let image = new Image();
+            image.src = albums.images[index];
+            image.onload = function () {
+                context.drawImage(image, i * image_width, j * image_height);
+            }
+            index++;
+        }
+    }
+
+    context.canvas.width = image_width * grid_size; 
+    context.canvas.height = image_height * grid_size; 
+}
+
 
 function request_chart_json() {
     let api_key = config.MY_KEY; 
-    let chart_option = undefined;
     let url = `http://ws.audioscrobbler.com/2.0/?method=user.getweeklyalbumchart&user=${username}&api_key=${api_key}&format=json`;
 
     $.getJSON(url, function(data) {
-        let weekly_albums = data.weeklyalbumchart.album;
-
-        for (let i = 0; i < grid_size*grid_size; i++) {
-            console.log(weekly_albums[i].name);
-        }
+        let chart_object = data.weeklyalbumchart.album;
+        build_chart_image(chart_object);
     });
 }
 
@@ -20,7 +58,6 @@ function request_chart_json() {
 function get_preferences() {
     username = document.getElementById("username").value;
     grid_size = document.getElementById("gridsize").value;
-    console.log(`${username} ${grid_size}`);
 }
 
 
