009b2838140924b50c19013c1710c6d307e58f76
Calandrinon <georgesut@yahoo.com>
2020-11-17 15:39:53 +0100

    Move column and row incrementation in the "then"
    BUG: More albums are displayed correctly, compared to the last commit, but not all of them are properly displayed

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ let grid_size = undefined;
 function build_chart_image(chart_object) {
     let canvas = document.getElementById("chart_canvas");
     let context = canvas.getContext("2d");
-    let albums = {
-        images: []
-    };
     let api_key = config.MY_KEY; 
 
     let dummy_image = new Image();
     dummy_image.src = "https://lastfm.freetls.fastly.net/i/u/174s/dfb8c764b71741e0c1652f197daacd4a.png";
     let image_width = dummy_image.width;
     let image_height = dummy_image.height;
+    let row = 0;
+    let column = 0;
     
     for (let album_index = 0; album_index < grid_size * grid_size; album_index++) {
         let album_cover = new Image();
@@ -25,24 +24,20 @@ function build_chart_image(chart_object) {
             then(data => {return data});
         
         album_art_link.then(function(result) {
-            albums.images.push(result.album.image[2]["#text"]);
-        });
-    } 
-
-    let index = 0;
-    console.log(albums.images);
-    console.log(albums.images.length);
-    console.log("wtf...");
-    for (let i = 0; i < grid_size; i++) {
-        for (let j = 0; j < grid_size; j++) {
             let image = new Image();
-            image.src = albums.images[index];
+            image.src = result.album.image[2]["#text"];
             image.onload = function () {
-                context.drawImage(image, i * image_width, j * image_height);
+                context.drawImage(image, row * image_height, column * image_width);
+                column++;
+                if (column == grid_size) {
+                    row++;
+                    column = 0;
+                }
             }
-            index++;
-        }
-    }
+            console.log(row, column);
+        });
+        
+    } 
 
     context.canvas.width = image_width * grid_size; 
     context.canvas.height = image_height * grid_size; 
